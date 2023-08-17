061e1e9530c3f9afacf82570f142722e7053b922
f89324 <f89324@gmail.com>
2023-08-18 01:27:17 +0300

    check if the settings are correct at startup

--- src/main/java/com/homer/config/HomerProperties.java
+++ src/main/java/com/homer/config/HomerProperties.java
@@ -1,13 +1,16 @@
 package com.homer.config;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Getter
+@Validated
 @Component
 @AllArgsConstructor
 @ConfigurationProperties(prefix = "homer")
@@ -18,16 +21,22 @@ public class HomerProperties {
     @Getter
     @AllArgsConstructor
     public static class MemberIntro {
+        @NotBlank
         private String username;
+        @NotBlank
         private String file;
     }
 
     @Getter
     @AllArgsConstructor
     public static class Reaction {
+        @NotBlank
         private String name;
+        @NotBlank
         private String file;
+        @NotBlank
         private String emoji;
+        @NotBlank
         private String description;
     }
 }
--- src/main/java/com/homer/listener/ReadyListener.java
+++ src/main/java/com/homer/listener/ReadyListener.java
@@ -1,5 +1,8 @@
 package com.homer.listener;
 
+import com.homer.config.HomerProperties;
+import com.homer.util.HomerUtil;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
@@ -9,11 +12,66 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@AllArgsConstructor
 public class ReadyListener implements EventListener {
+
+    private final HomerProperties homerProperties;
 
     @Override
     public void onEvent(@NotNull GenericEvent event) {
-        if (event instanceof ReadyEvent)
+        if (event instanceof ReadyEvent) {
+            validateConfig();
             log.info("Homer is ready!");
+        }
+    }
+
+    private void validateConfig() {
+        log.info("start of config validation");
+
+        String errors = "";
+
+        errors += checkIntroAudioFilesExisting();
+        errors += checkReactionAudioFilesExisting();
+
+        if (!errors.isBlank()) {
+            log.error("Errors in config:" + errors);
+        }
+
+        log.info("end of config validation");
+
+        if (!errors.isBlank()) {
+            log.error("There are errors in the application configuration!");
+            System.exit(0);
+        }
+    }
+
+    @NotNull
+    private String checkIntroAudioFilesExisting() {
+        StringBuilder errorsBuilder = new StringBuilder();
+
+        homerProperties.getIntro().forEach(intro -> {
+            try {
+                HomerUtil.getFile(intro.getFile());
+            } catch (Exception e) {
+                errorsBuilder.append("\n * File [").append(intro.getFile()).append("] not found!");
+            }
+        });
+
+        return errorsBuilder.toString();
+    }
+
+    @NotNull
+    private String checkReactionAudioFilesExisting() {
+        StringBuilder errorsBuilder = new StringBuilder();
+
+        homerProperties.getReactions().forEach(reaction -> {
+            try {
+                HomerUtil.getFile(reaction.getFile());
+            } catch (Exception e) {
+                errorsBuilder.append("\n * File [").append(reaction.getFile()).append("] not found!");
+            }
+        });
+
+        return errorsBuilder.toString();
     }
 }
